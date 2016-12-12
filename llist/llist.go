e3db3315695d42b4f5d12da05e368588220657e0
cometeor <cometliao@gmail.com>
2016-12-12 12:29:29 +0800

    Add Length() and Remove()

--- llist/llist.go
+++ llist/llist.go
@@ -11,6 +11,7 @@ import (
 type List struct {
 	head linked.Noder
 	tail linked.Noder
+	length uint
 }
 
 // Get the head node.
@@ -21,6 +22,11 @@ func (l *List) Head() linked.Noder {
 // Get the tail node.
 func (l *List) Tail() linked.Noder {
 	return l.tail
+}
+
+// Get length.
+func (l *List) Length() uint {
+	return l.length
 }
 
 // Append adds the given node as the tail of the list.
@@ -34,6 +40,7 @@ func (l *List) Append(n linked.Noder) (err error) {
 	}
 
 	l.tail = n
+	l.length++
 	return err
 }
 
@@ -48,6 +55,7 @@ func (l *List) Push(n linked.Noder) (err error) {
 	}
 
 	l.head = n
+	l.length++
 	return err
 }
 
@@ -63,7 +71,34 @@ func (l *List) Pop() (n linked.Noder, err error) {
 		newHead.SetPrev(n)
 	}
 	l.head = newHead
+	l.length--
 	return oldHead, err
+}
+
+// Remove node.
+func (l *List) Remove(n linked.Noder) error {
+	prev, isFirst, _ := n.Prev()
+	next, isLast, _ := n.Next()
+	if (isFirst) {
+		if (n != l.head) {
+			return errors.New("Invalid node.")
+		}
+		l.head = next
+	} else {
+		prev.SetNext(next)
+	}
+	if (isLast) {
+		if (n != l.tail) {
+			return errors.New("Invalid node.")
+		}
+		l.tail = prev
+	} else {
+		next.SetPrev(prev)
+	}
+	n.SetNext(nil)
+	n.SetPrev(nil)
+	l.length--
+	return nil
 }
 
 // Reverse reverses the current linked list in place.
